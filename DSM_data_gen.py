af40d3538706a1ab72817764807f770e20d878bc
felipekriffel <felipekriffel@gmail.com>
2025-09-12 17:07:21 -0300

    Nova geração de dados, README

---
+++ DSM_data_gen.py
@@ -0,0 +1,129 @@
+import eitx
+import scipy as sp
+import numpy as np
+import dolfinx
+from petsc4py import PETSc
+import json
+import os
+import sys
+
+def main(SETTINGS_JSON):
+
+  settings = json.loads(SETTINGS_JSON)
+  
+  if not os.path.isdir(settings['dsm_datapath']):
+      os.mkdir(settings['dsm_datapath'])
+
+  with open(settings['dsm_datapath']+"/data_info.json","w") as f:
+    f.write(json.dumps(settings))
+
+  "Importing modules"
+  import logging
+  # Set the logging level to suppress most logs
+  logging.getLogger('UFL_LEGACY').setLevel(logging.WARNING)
+  logging.getLogger('dolfin').setLevel(logging.WARNING)
+  logging.getLogger('PETSc').setLevel(logging.WARNING)
+  logging.getLogger('FFC').setLevel(logging.WARNING)
+
+
+  #Loading kit4 data (somente para definir a corrente de maneira correta)
+  mat = sp.io.loadmat("fin_data/datamat/datamat_1_2")
+  CP = mat.get("CurrentPattern").T
+
+  #Current
+  I_all=CP[-15:][settings['currents']]/np.sqrt(2)
+  l, L=np.shape(I_all) #Number of experiments = 15, Number of Electrodes = 16
+  # print(I_all)# MESH (For real data)
+
+  "Basic Definitions"
+  radius=1               #Circle radius
+  per_cober=0.454728409  #Percentage of area covered by electrodes
+  rotate=0               #Electrodes Rotation
+  z=np.ones(L)*0.07858  
+
+  'Return object with angular position of each electrode'
+  ele_pos = eitx.Electrodes(L, per_cober, rotate)
+
+  'Mesh'
+  # mesh_inverse=MyMesh(radius, refine_n, n_in, n_out, ele_pos)
+  mesh_object = eitx.MeshClass(ele_pos,0.4,0.6)
+
+  ## Direct problem
+  dir_problem = eitx.DirectProblem(mesh_object,z)
+  V0 = dir_problem.V0   # Discontinuous Garlekin space function
+  V = dir_problem.V     # Continuous Garlekin space function
+
+  # 'Plot'
+
+  #"Define gamma as constant = Background"
+  bg = settings['bg']
+  ivhigh,ivlow = settings['ivhigh'], settings['ivlow']
+  gamma0 = dolfinx.fem.Function(V0)
+  gamma0.x.array[:] = bg
+
+  #Solving Forward Problem
+  list_u, list_U0_m = dir_problem.solve_problem_current(I_all, gamma0)
+  list_U0 = np.array(list_U0_m).flatten()
+
+  'Retangular Mesh'
+  N = settings["N"]                     # grid with N*N points (works well with 0 < N < 400)
+  h = 2*radius/(N-1)                    # step size
+  x = [radius - i*h for i in range(N)]  # x grid points
+  y = [-radius + i*h for i in range(N)] # y grid points
+
+  # MESH x and y
+  mesh_x = np.zeros((N,N))              # x-Data (input of CNN)
+  mesh_y = np.zeros((N,N))              # y-Data (input of CNN)
+  for i in range(N):
+    for j in range(N):
+      mesh_x[i][j] = x[i]
+      mesh_y[i][j] = y[j]
+
+  gamma = dolfinx.fem.Function(V0)      # Empty function
+  T1 = []                               # To save data
+
+  samples_dir = settings['samples_dir']
+  samples_names = [file for file in os.listdir(samples_dir) if file.endswith(".npy")]
+
+  # Loop for generating data
+  noise_level = settings["noise_level"] # % of artificial noise in data
+  for sample in samples_names:
+  
+    gamma.x.array[:]= np.load(os.path.join(samples_dir, sample))
+
+    "Define data in a homogeneus grid for training"
+    A = eitx.genGammaImg(gamma,mesh_x,mesh_y,bg,ivhigh,ivlow)
+
+    "Solve Forward Problem with Background + Inclusion"
+    list_u1, list_U1_m = dir_problem.solve_problem_current(I_all, gamma)
+
+    "Difference of Resulting Potentials"
+    differ = np.array(list_U1_m) - np.array(list_U0_m)
+    noise = np.random.uniform(-1, 1, size=(len(differ),len(differ[0])))
+    noise = noise / np.linalg.norm(noise)
+    differ_noisy = differ + noise_level*noise*np.linalg.norm(differ)
+
+    "Solve Forward Problem with Background and Difference of Potentials as Currents"
+    list_ur_dif, list_U_dif = dir_problem.solve_problem_current(differ_noisy, gamma0)
+
+    "Saves data on tensor"
+    T = np.zeros((l + 3,N,N))
+    for k in range(l):
+      T[k] = eitx.genPotentialImg(list_ur_dif[k],mesh_x,mesh_y,bg)
+
+    T[l] = mesh_x
+    T[l+1] = mesh_y
+    T[l+2] = A
+    np.save(f"{settings['dsm_datapath']}/{sample}_dsm",T)
+
+
+  # np.save('EIT_Data_for_CNN', T1)
+  print(f'Data saved at {settings["dsm_datapath"]}.')
+
+if __name__=="__main__":
+  SETTINGS_JSON = sys.argv[1]
+  if SETTINGS_JSON.endswith('.json') and os.path.isfile(SETTINGS_JSON):
+    with open(SETTINGS_JSON) as f:
+      SETTINGS_JSON = f.read()
+      
+  main(SETTINGS_JSON)
--- README.md
+++ README.md
@@ -1,3 +1,44 @@
 # CEM_CNN
 
-Testing Neural Networks for Electrical Impedance Tomography image reconstruction.
+Testes de redes neurais na reconstrução de imagens de Tomografia por Impedância.
+
+## Ramo `main`
+
+Arquivos para implementação dos testes com o método Deep-DSM.
+
+
+### `CNN_routine.py`
+
+Faz a rotina de geração, treino e teste de acordo com os arquivos de configuração informados.
+
+### `DSM_test_kit4.py'`
+
+Carrega uma rede treinada e testa nas amostras do conjunto de dados KIT4. 
+
+Argumentos: 
+- `results_path`: caminho para pasta com a rede treinada e onde o resultado do teste será salvo.
+
+
+### `CEM_data_gen.py`
+
+Gera dados aleatórios de condutividade, salvando os vetores de coeficientes e as informações usadas para gerar cada imagem.
+
+### `DSM_data_gen`
+
+Gera dados com as diferenças de Cuachy para o método DSM, usando como base as condutividades previamente geradas.
+
+### `CNN_test_checkpoints`
+
+Usado para treinar a UNET
+
+### `eitx.py`
+
+Arquivo com as funções usadas na implementação da Tomografia por Impedância (Modelo Completo de Eletrodos)
+
+### `UNET_train.py`
+
+Treinamento da rede UNET definida.
+
+### `unet.py`
+
+Arquivo com a classe da rede UNET usada.
